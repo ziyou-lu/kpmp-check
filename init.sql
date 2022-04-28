a3afa2cecfd2bc135be647c39d15cd6364e2fc69
luwangda <luwangda@kedacom.com>
2022-04-28 18:11:10 +0800

    create index

--- init.sql
+++ init.sql
@@ -1,8 +1,11 @@
 drop table if exists kpmp_analysis_kdstaticface ;
-create table kpmp_analysis_kdstaticface (
-	id SERIAL primary key,
-	FaceID varchar(64),
-	ShotTime date,
-	FeatureData varchar(1024),
-	del int
+CREATE TABLE kpmp_analysis_kdstaticface (
+                                                   id serial4 NOT NULL,
+                                                   faceid varchar(64) NULL,
+                                                   shottime timestamptz NULL,
+                                                   del int4 NULL,
+                                                   featuredata varchar(1024) NULL,
+                                                   CONSTRAINT kpmp_analysis_kdstaticface_pkey PRIMARY KEY (id)
 );
+CREATE INDEX kpmp_analysis_kdstaticface_faceid_idx ON kpmp_analysis_kdstaticface USING btree (faceid, shottime);
+CREATE INDEX kpmp_analysis_kdstaticface_shottime_idx ON kpmp_analysis_kdstaticface USING btree (shottime);
